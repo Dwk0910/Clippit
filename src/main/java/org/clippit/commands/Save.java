75aa82ecd1c553aeb7b14f535217f36cdfb242e5
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-04 23:34:22 +0900

    Fix: File collecting algorithm changed

--- src/main/java/org/clippit/commands/Save.java
+++ src/main/java/org/clippit/commands/Save.java
@@ -1,47 +1,46 @@
 package org.clippit.commands;
 
-import net.lingala.zip4j.model.enums.EncryptionMethod;
 import org.clippit.Clippit;
 import org.clippit.ClippitException;
-import org.clippit.Util;
 import org.clippit.annotations.RequiresArgument;
 
 import java.io.File;
 import java.io.IOException;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-import java.util.Objects;
-import java.util.Arrays;
+import java.nio.file.Paths;
 
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.CompressionMethod;
+import net.lingala.zip4j.exception.ZipException;
 
 @RequiresArgument(count = 2)
 public class Save implements Clippit.Command {
     @Override
     public void run(String... argv) {
-        File f = new File(argv[0]);
-        if (!f.exists()) throw new ClippitException("File does not exist: %s".formatted(argv[0]));
+        Path path = Paths.get(argv[1]);
+        if (!path.toFile().exists()) throw new ClippitException("%s: Not valid file or directory.".formatted(argv[1]));
 
-        ZipParameters parameters = new ZipParameters();
-        parameters.setCompressionMethod(CompressionMethod.DEFLATE);
-        parameters.setEncryptFiles(true);
-        parameters.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
-
-        if (f.isDirectory()) {
-            File[] items = Objects.requireNonNull(f.listFiles());
-            try (ZipFile zipFile = new ZipFile(Path.of(Clippit.templateDir.toString(), "%s.zip".formatted(argv[1])).toString())) {
-                if (items.length == 0 && Util.ask("Directory %s has no any files. Do you want to save empty directory?")) {
-                    zipFile.addFolder(f);
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(path);
+             ZipFile zipFile = new ZipFile(Path.of(Clippit.templateDir.toString(), argv[0] + ".zip").toString())
+        ) {
+            ZipParameters parameters = new ZipParameters();
+            parameters.setCompressionMethod(CompressionMethod.DEFLATE);
+            dirStream.forEach(path_ -> {
+                try {
+                    File f = path_.toFile();
+                    if (f.isDirectory()) zipFile.addFolder(f, parameters);
+                    else zipFile.addFile(f, parameters);
+                } catch (ZipException e) {
+                    System.out.println(e.getMessage());
                 }
-                zipFile.addFiles(Arrays.stream(items).toList(), parameters);
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        } else {
-            System.out.println("File");
+            });
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
