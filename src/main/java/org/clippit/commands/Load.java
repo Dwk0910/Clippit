2c1b79e98ded22851f3f834fe25bad04d50bb308
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-05 18:53:43 +0900

    Feature: conflict detecting feature

--- src/main/java/org/clippit/commands/Load.java
+++ src/main/java/org/clippit/commands/Load.java
@@ -1,16 +1,22 @@
 package org.clippit.commands;
 
 import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.FileHeader;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 import org.clippit.Clippit;
 import org.clippit.ClippitException;
+import org.clippit.ColorText;
 import org.clippit.util.Util;
 import org.clippit.annotations.RequiresArgument;
 
 import java.io.File;
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RequiresArgument
 public class Load implements Clippit.Command {
@@ -25,10 +31,30 @@ public class Load implements Clippit.Command {
 
             if (!targetFolder.exists() && Util.ask("Target folder '%s' is not exist. Do you want to create a new directrory?".formatted(argv[1])))
                 FileUtils.forceMkdir(targetFolder);
-            else if (!targetFolder.isDirectory()) throw new ClippitException("Target folder is not a directory");
+            else if (!targetFolder.isDirectory())
+                throw new ClippitException("'%s' is not a directory".formatted(argv[1]));
 
             try (ZipFile zipFile = new ZipFile(template)) {
-                zipFile.extractAll(targetFolder.getAbsolutePath());
+                File[] fileList = targetFolder.listFiles();
+                if (fileList != null && fileList.length > 0) {
+                    java.util.List<String> original_names = new ArrayList<>();
+                    Arrays.stream(fileList).forEach(file -> original_names.add(FilenameUtils.getName(file.toPath().toString())));
+
+                    java.util.List<String> conflicts = new ArrayList<>();
+                    for (FileHeader i : zipFile.getFileHeaders()) {
+                        if (original_names.contains(i.getFileName()))
+                            conflicts.add(i.getFileName());
+                    }
+
+                    if (!conflicts.isEmpty()) {
+                        System.out.println(ColorText.text("Following files have conflicts:", "", "none", true, false, false));
+                        for (String name : conflicts) {
+                            System.out.println("- " + name);
+                        }
+                        if (!Util.ask("Overwrite?")) throw new ClippitException("Action cancelled.");
+                    }
+                }
+                zipFile.extractAll(targetFolder.getPath());
             }
 
             System.out.printf("Template '%s' has been successfully loaded into directory '%s'.%n", argv[0], targetFolder.getPath());
