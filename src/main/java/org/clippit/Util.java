b5229fb1ee5b2eec8c9c3e2b80f2aecf31304a0f
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-05 10:37:40 +0900

    Feature: list command added

--- src/main/java/org/clippit/Util.java
+++ src/main/java/org/clippit/Util.java
@@ -1,12 +1,27 @@
 package org.clippit;
 
+import org.apache.commons.io.FileUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 import java.nio.file.Path;
 
 public class Util {
+    private static void doRecovery() {
+        try {
+            File f = Clippit.templateDir.toFile();
+            if (!f.exists()) FileUtils.touch(f);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static boolean ask(String question) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.print(ColorText.text(question + " (yes/no): ", "yellow", "none", true, false, false));
@@ -20,7 +35,42 @@ public class Util {
     }
 
     public static boolean isExist(String name) {
+        doRecovery();
         File f = new File(Path.of(Clippit.templateDir.toString(), name + ".zip").toString());
+        if (!f.exists()) return false;
         return f.exists();
     }
+
+    public static List<File> getTemplates() {
+        List<File> result = new ArrayList<>();
+        File f = Clippit.templateDir.toFile();
+        if (!f.exists()) {
+            doRecovery();
+            return result;
+        }
+
+        File[] templates = f.listFiles();
+        if (templates == null) return result;
+
+        for (File item : templates) {
+            if (item.getName().endsWith(".zip")) result.add(item);
+        }
+        return result;
+    }
+
+    public static String getReadableSize(long size) {
+        String[] units = {"B", "KB", "MB", "GB", "TB", "PB"};
+        if (size < 1000) return size + " B";
+
+        int unitIndex = 0;
+        double displaySize = size;
+
+        while (displaySize >= 1000 && unitIndex < units.length - 1) {
+            displaySize /= 1000.0;
+            unitIndex++;
+        }
+
+        return String.format("%.1f %s", displaySize, units[unitIndex]);
+    }
+
 }
