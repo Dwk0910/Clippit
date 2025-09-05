27dbb5a93fefcdc4b5b1cc611b67b45d32eddc2c
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-05 16:29:24 +0900

    Fix: ArrayIndexOutOfBoundException has been fixed

--- src/main/java/org/clippit/commands/Load.java
+++ src/main/java/org/clippit/commands/Load.java
@@ -20,7 +20,9 @@ public class Load implements Clippit.Command {
             File template = Util.getTemplate(argv[0]);
             if (template == null) throw new ClippitException("Template %s is not exist.".formatted(argv[0]));
 
-            File targetFolder = new File(argv[1]);
+            File targetFolder = new File(".");
+            if (argv.length >= 2) targetFolder = new File(argv[1]);
+
             if (!targetFolder.exists() && Util.ask("Target folder '%s' is not exist. Do you want to create a new directrory?".formatted(argv[1])))
                 FileUtils.forceMkdir(targetFolder);
             else if (!targetFolder.isDirectory()) throw new ClippitException("Target folder is not a directory");
